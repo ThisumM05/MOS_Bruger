27f4f5e83e27e54143bc92315fa4f446ced82c67
ThisumM05 <thisummadappuli@gmail.com>
2026-03-17 03:11:21 +0530

    new

--- mos_product_frontend/most_product_frontend/src/main.jsx
+++ mos_product_frontend/most_product_frontend/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import axios from 'axios';
 import { store } from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App.jsx';
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+
+axios.interceptors.request.use((config) => {
+  if (typeof config.url === 'string' && config.url.startsWith('http://localhost:8000')) {
+    config.url = config.url.replace('http://localhost:8000', API_BASE_URL);
+  }
+  return config;
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
